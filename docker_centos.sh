a603833002f4b64fbcfcb593f1f10374c7bdc470
As'ad Tahta Alfina <57313918+asadtahta95@users.noreply.github.com>
2022-08-08 09:17:00 +0700

    Update docker_centos.sh

--- docker_centos.sh
+++ docker_centos.sh
@@ -1,32 +1,14 @@
-# Instal dependency
-dnf install -y dnf-utils device-mapper-persistent-data lvm2 fuse-overlayfs wget
-
-# Add docker-ce repository for centos
-yum install -y yum-utils
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-
-# Install Docker CE
+# Update packages
+yum update -y
+# Install Docker
+# Add repo
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# remove podman dan buildah
+dnf remove podman buildah
+# Install packages Docker
 dnf install docker-ce docker-ce-cli containerd.io
-
-# Jalankan service docker
-systemctl enable --now docker
-
-# Enable firewalld
-systemctl enable firewalld
-
-# start firewalld
-systemctl start firewalld
-
-# Allows container to container communication, the solution to the problem
-firewall-cmd --zone=public --add-masquerade --permanent
-
-# Allow port 2375 expose to outside network
-firewall-cmd --zone=public --add-port=2375/tcp --permanent
-
-# reload the firewall
-firewall-cmd --reload
-
-# Restart service docker
-systemctl restart docker.service
+# Enable & start docker service
+systemctl start docker.service
+systemctl enable docker.service
+# Run docker without root
+usermod -aG docker $USER
